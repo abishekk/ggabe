ad74000cac75b79657662bd1a23e660f6cdfb498
Howard C Lee <howardchlee@gmail.com>
2013-11-29 21:47:10 -0800

    Fix solution for larger input

--- learn/gmp_mult/howard/mult.cpp
+++ learn/gmp_mult/howard/mult.cpp
@@ -8,12 +8,8 @@ int main()
 {
 	mpz_t a, b, c;
 	int inta, intb;
-	cin >> inta >> intb;
-//	cout << "initializing \n";
-	mpz_init_set_ui(a, inta);
-	mpz_init_set_ui(b, intb);
-//	cout << "multiplying\n";
-	mpz_init(c);
+	mpz_inits(a, b, c, NULL);
+	gmp_scanf("%Zd %Zd", a, b);
 	mpz_mul(c, a, b);
 	gmp_printf("%Zd\n", c);
 	return 0;
